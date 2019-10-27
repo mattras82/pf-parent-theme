5e53ee81b995ff9059adad05cbf0d22d2b399d3a
mattras82 <mattras82@gmail.com>
2019-10-27 13:56:19 -0600

    Fixes & enhancements from webpack build process

--- build/util/production.js
+++ build/util/production.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (env, argv, home) => {
   // PLUGINS
   const OptimizeCssAssetsPlugin = require(home + '/node_modules/optimize-css-assets-webpack-plugin');
@@ -9,14 +12,27 @@ module.exports = (env, argv, home) => {
   const ManifestTransform = require('../manifest-transformer');
   const OfflineTransform = require('../offline-transformer');
 
+  let prefix = '../pf-parent-theme';
+  let copyFiles = [
+    {from: '/build/manifest.json', transform(content) { return ManifestTransform(content, home) }},
+    {from: '/build/sw.js', transform(content) { return SWTransform(content) }},
+    {from: '/build/offline.html', transform(content) { return OfflineTransform(content) }},
+  ];
+
+  copyFiles = copyFiles.map(item => {
+    try {
+      let childFileExists = fs.readFileSync(home + item.from);
+      item.from = home + item.from;
+    } catch(e) {
+      item.from = prefix + item.from;
+    }
+    return item;
+  });
+
   return  {
     plugins: [
       new PFNotificationPlugin(home),
-      new CopyWebpackPlugin([
-        { from: '../pf-parent-theme/build/manifest.json', transform(content) { return ManifestTransform(content, home) } },
-        { from: '../pf-parent-theme/build/sw.js', to: '../../../../', transform(content) { return SWTransform(content) } },
-        { from: '../pf-parent-theme/build/offline.html', to: '../../../../', transform(content) { return OfflineTransform(content) } },
-      ]),
+      new CopyWebpackPlugin(copyFiles),
       new OptimizeCssAssetsPlugin({
         cssProcessorOptions: { discardComments: { removeAll: true } },
       }),
